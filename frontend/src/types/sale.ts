bd0b129da48185c429e46e9b2066ee4ef5af13dd
matheus-feliz <matheusfoliveira172@gmail.com>
2021-05-09 20:40:42 -0300

    BarChart integration(anonymous function)

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellername: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
